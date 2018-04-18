69f8f8f3b225e6307edf5faf3b06d45f4b23d8a1
technolog <viktor@allz.su>
2018-04-19 01:22:33 +0300

    move setting button

--- ledger-worker.js
+++ ledger-worker.js
@@ -1,17 +1,19 @@
+/**
 'use strict'
 const ledger = require('ledgerco')
 
 let connected = false
-/*
+
 setInterval(() => {
   ledger.comm_node.list_async().then((deviceList) => {
     connected = deviceList.length > 0
     process.send({connected: connected})
   })
 }, connected ? 5000 : 1000)
-*/
+
 process.on('message', (message) => {
   if (message.quit) {
     process.exit()
   }
 })
+*/
